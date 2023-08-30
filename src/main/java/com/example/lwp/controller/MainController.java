81d636a09a64a11ba70c02e7aa0d1c47940b7a1c
Singe00 <singery00@gmail.com>
2023-08-30 17:33:24 +0900

    layout complete

--- src/main/java/com/example/lwp/controller/MainController.java
+++ src/main/java/com/example/lwp/controller/MainController.java
@@ -1,16 +1,27 @@
 package com.example.lwp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/main")
-    public String main(@RequestParam(name = "name",required = false,defaultValue = "World") String name, Model model){
-        model.addAttribute("name",name);
-        return "main";
+    @GetMapping("/")
+    public String main(){
+        return "view/main";
+    }
+
+    @GetMapping("/t")
+    public String test(){
+        return "view/content";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "view/login";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "view/about";
     }
 }
