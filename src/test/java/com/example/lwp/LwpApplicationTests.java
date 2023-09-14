fef59dd06aa2dc41097b1edc66d4fa58587daacf
Singe00 <singery00@gmail.com>
2023-09-14 21:56:12 +0900

    전적 검색 기능

--- src/test/java/com/example/lwp/LwpApplicationTests.java
+++ src/test/java/com/example/lwp/LwpApplicationTests.java
@@ -12,16 +12,5 @@ import java.util.List;
 @SpringBootTest
 class LwpApplicationTests {
 
-	@Autowired
-	private ApiService apiService;
-	@Test
-	void contextLoads() {
-		List<MatchDto> a = new ArrayList<>();
-		a = apiService.FindMatch("뷔빅휴칙휜");
-
-		System.out.println(a.get(0).getInfo().getParticipants().get(0));
-		System.out.println(a.get(0).getInfo().getParticipants().get(1));
-		System.out.println(a.get(0).getInfo().getParticipants().get(2));
-	}
 
 }
