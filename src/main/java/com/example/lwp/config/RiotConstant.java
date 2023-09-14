c24c12b224f663c2a42348b634b30b92074aaf35
Singe00 <singery00@gmail.com>
2023-09-14 21:46:34 +0900

    전적 검색 기능

--- src/main/java/com/example/lwp/config/RiotConstant.java
+++ src/main/java/com/example/lwp/config/RiotConstant.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component("RiotConstant")
 public class RiotConstant {
 
-    public static final String API_KEY = "your api key input";
+    public static final String API_KEY = "RGAPI-d7c9e381-28c4-4184-a497-5cab11336abb";
 
 }
