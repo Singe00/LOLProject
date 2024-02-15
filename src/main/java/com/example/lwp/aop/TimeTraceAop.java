1846c0136005fd6d96c53e34fc9fd2350c578fb2
Singe00 <singery00@gmail.com>
2024-02-15 19:07:39 +0900

    변경점 적용

--- src/main/java/com/example/lwp/aop/TimeTraceAop.java
+++ src/main/java/com/example/lwp/aop/TimeTraceAop.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TimeTraceAop {
 
-    @Around("execution(* com.example..*(..))")
+    @Around("execution(* com.example.lwp.service..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
         long start = System.currentTimeMillis();
         System.out.println("START: "+joinPoint.toString());
